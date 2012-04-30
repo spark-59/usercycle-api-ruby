c0af818727d12b04d472b9c26b409398d2b446cc
Steve Odom <steve.odom@gmail.com>
2012-04-30 14:57:43 -0500

    Changed the default host to nil instead of empty quotes. The default host was not being triggered.

--- lib/usercycle.rb
+++ lib/usercycle.rb
@@ -7,7 +7,7 @@ module Usercycle
   
   class Client
     
-    def initialize(key, host = "")
+    def initialize(key, host = nil)
       @api_key = key
       self.class.headers 'X-Usercycle-API-Key' => @api_key
       self.class.base_uri host || "api.usercycle.com/api/v1"
