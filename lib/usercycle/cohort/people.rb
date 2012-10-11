099b29a344e695a402cfa8e729b77d81c0fd9a87
Lukas Fittl <lukas@fittl.com>
2012-10-11 21:51:36 +0200

    Upgrade cohort people class to new API.

--- lib/usercycle/cohort/people.rb
+++ lib/usercycle/cohort/people.rb
@@ -18,17 +18,17 @@ module Usercycle
 
         def get_daily(start_date, macro = 'AARRR', event = 'signed_up')
           date = Usercycle::Cohort::FormatDate.new(start_date).get
-          @client.class.get("/cohorts/daily/#{date}/people.json?macro=#{macro}&event=#{event}")
+          @client.class.get("/cohorts/daily/#{date}.json?macro=#{macro}&event=#{event}&load_people=true")
         end
 
         def get_weekly(start_date, macro = 'AARRR', event = 'signed_up')
           date = Usercycle::Cohort::FormatDate.new(Usercycle::Cohort::GetPreviousMonday.new(start_date).get).get
-          @client.class.get("/cohorts/weekly/#{date}/people.json?macro=#{macro}&event=#{event}")
+          @client.class.get("/cohorts/weekly/#{date}/people.json?macro=#{macro}&event=#{event}&load_people=true")
         end
 
         def get_monthly(start_date, macro = 'AARRR', event = 'signed_up')
           date = Usercycle::Cohort::FormatDate.new(Usercycle::Cohort::GetFirstOfMonth.new(start_date).get).get
-          @client.class.get("/cohorts/monthly/#{date}/people.json?macro=#{macro}&event=#{event}")
+          @client.class.get("/cohorts/monthly/#{date}/people.json?macro=#{macro}&event=#{event}&load_people=true")
         end
       end
     end
